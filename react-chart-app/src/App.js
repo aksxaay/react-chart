b4bed40ddfb63c72ad340d9e2b155db614d4355e
Akshay Kumar <akshaykumarragavan@gmail.com>
2022-04-11 23:26:50 +0530

    testing @tailwindcss + prettierconfig which somehow works

--- react-chart-app/src/App.js
+++ react-chart-app/src/App.js
@@ -10,9 +10,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold">Hello world!</h1>
 
-        <div class="main-title">Sunburst Chart</div>
+        <div class="main-title ml-4 flex h-24 border-2 border-gray-300 p-4 text-gray-50 shadow-md">
+          Sunburst Chart
+        </div>
 
         <div className="App">
           <h3>This is a new chart</h3>
--- react-chart-app/tailwind.config.js
+++ react-chart-app/tailwind.config.js
@@ -4,4 +4,5 @@ module.exports = {
     extend: {}
   },
   plugins: []
+  // tailwindConfig: "./styles/tailwind.config.js"
 };
